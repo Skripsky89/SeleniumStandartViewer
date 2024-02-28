4f5ab05e50153498b3addf44cb5e4c86ea479852
Michal Skripsky <skrimi00gmail.com>
2024-02-28 14:21:14 +0100

    ZobrazenÃiElementu

--- src/test/java/seleniumReworkBetter/ZakladKopirovani.java
+++ src/test/java/seleniumReworkBetter/ZakladKopirovani.java
@@ -1,10 +1,17 @@
 package seleniumReworkBetter;
 
-import org.junit.After;
-import org.junit.Test;
+import org.junit.Assert;
 import org.junit.Before;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.Test;
+import org.junit.After;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.lang.Thread;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class ZakladKopirovani {
 
