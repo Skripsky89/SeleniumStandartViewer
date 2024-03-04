f66ec7b7ee301df6beb1864df8aa4a697ca8a12c
Michal Skripsky <skrimi00gmail.com>
2024-03-04 18:15:50 +0100

    KonecneNecoNaUrovni

--- src/test/java/seleniumReworkBetter/RegistracniFormular.java
+++ src/test/java/seleniumReworkBetter/RegistracniFormular.java
@@ -1,22 +1,25 @@
 package seleniumReworkBetter;
 
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.Assert;
+
+
 public class RegistracniFormular {
 
     private WebDriver driver;
     private final String BASE_URL = "http://localhost/registracia.php";
-    private String Email ="nova@gmail.com";
-    private String Jmeno = "Novak";
-    private String Prijmení = "Karel";
-    private String Heslo = "fafawrf2378fd";
-    private String Reheslo = "fafawrf2378fd";
+    private final String Email ="nova@gmail.com";
+    private final String Jmeno = "Novak";
+    private final String Prijmeni = "Karel";
+    private final String Heslo = "fafawrf2378fd";
+    private final String Reheslo = "fafawrf2378fd";
 
     @Before
     public void setUp (){
@@ -28,145 +31,102 @@ public class RegistracniFormular {
     @Test
     public void testCheckbox()  {
 
-        WebElement Pemail = driver.findElement(By.xpath("//input[@name='email']"));
-        WebElement Pjmeno = driver.findElement(By.xpath("//input[@name='name']"));
-        WebElement Pprimeni = driver.findElement(By.xpath("//input[@name='surname']"));
-        WebElement Pheslo = driver.findElement(By.xpath("//input[@name='password']"));
-        WebElement Preheslo = driver.findElement(By.xpath("//input[@name='password-repeat']"));
-        WebElement SomRobot = driver.findElement(By.id("checkbox"));
-        WebElement Pregistrace = driver.findElement(By.xpath("//button[@class='btn btn-success btn-lg btn-block']"));
-        SomRobot.click();
-        Pregistrace.click();
+
+        seleniumReworkBetter.RegistracniFormular.Result result = getResult();
+        result.SomRobot.click();
+        result.Pregistrace.click();
 
         WebElement Vysledek = driver.findElement(By.xpath("//div[contains(@class, 'alert-danger')]"));
 
         String TextVysledku = Vysledek.getText();
         Assert.assertEquals(TextVysledku,"Registracia neuspesna!" );
 
-    } @Test
+    }
+    @Test
     public void testName (){
 
-        WebElement Pemail = driver.findElement(By.xpath("//input[@name='email']"));
-        WebElement Pjmeno = driver.findElement(By.xpath("//input[@name='name']"));
-        WebElement Pprimeni = driver.findElement(By.xpath("//input[@name='surname']"));
-        WebElement Pheslo = driver.findElement(By.xpath("//input[@name='password']"));
-        WebElement Preheslo = driver.findElement(By.xpath("//input[@name='password-repeat']"));
-        WebElement SomRobot = driver.findElement(By.id("checkbox"));
-        WebElement Pregistrace = driver.findElement(By.xpath("//button[@class='btn btn-success btn-lg btn-block']"));
 
-        Pjmeno.sendKeys("");
-        SomRobot.click();
+        seleniumReworkBetter.RegistracniFormular.Result result = getResult();
 
-        Pregistrace.click();
+        result.Pjmeno.sendKeys(Jmeno);
+        result.SomRobot.click();
+        result.Pregistrace.click();
         WebElement Vysledek = driver.findElement(By.xpath("//div[contains(@class, 'alert-danger')]"));
         String TextVysledku = Vysledek.getText();
 
         Assert.assertEquals(TextVysledku,"Registracia neuspesna!" );
 
-    } @Test
+    }
+    @Test
     public void testEmail (){
 
-        WebElement Pemail = driver.findElement(By.xpath("//input[@name='email']"));
-        WebElement Pjmeno = driver.findElement(By.xpath("//input[@name='name']"));
-        WebElement Pprimeni = driver.findElement(By.xpath("//input[@name='surname']"));
-        WebElement Pheslo = driver.findElement(By.xpath("//input[@name='password']"));
-        WebElement Preheslo = driver.findElement(By.xpath("//input[@name='password-repeat']"));
-        WebElement SomRobot = driver.findElement(By.id("checkbox"));
-        WebElement Pregistrace = driver.findElement(By.xpath("//button[@class='btn btn-success btn-lg btn-block']"));
+        seleniumReworkBetter.RegistracniFormular.Result result = getResult();
 
-        Pemail.sendKeys(Email);
-        SomRobot.click();
-        Pregistrace.click();
-
+        result.Pemail.sendKeys(Email);
+        result.SomRobot.click();
+        result.Pregistrace.click();
         WebElement Vysledek = driver.findElement(By.xpath("//div[contains(@class, 'alert-danger')]"));
-
         String TextVysledku = Vysledek.getText();
+
         Assert.assertEquals(TextVysledku,"Registracia neuspesna!" );
 
-    } @Test
+    }
+    @Test
     public void testPrijmeni (){
 
-        WebElement Pemail = driver.findElement(By.xpath("//input[@name='email']"));
-        WebElement Pjmeno = driver.findElement(By.xpath("//input[@name='name']"));
-        WebElement Pprimeni = driver.findElement(By.xpath("//input[@name='surname']"));
-        WebElement Pheslo = driver.findElement(By.xpath("//input[@name='password']"));
-        WebElement Preheslo = driver.findElement(By.xpath("//input[@name='password-repeat']"));
-        WebElement SomRobot = driver.findElement(By.id("checkbox"));
-        WebElement Pregistrace = driver.findElement(By.xpath("//button[@class='btn btn-success btn-lg btn-block']"));
+        seleniumReworkBetter.RegistracniFormular.Result result = getResult();
 
-        Pprimeni.sendKeys(Prijmení);
-        SomRobot.click();
-        Pregistrace.click();
-
+        result.Pprimeni.sendKeys(Prijmeni);
+        result.SomRobot.click();
+        result.Pregistrace.click();
         WebElement Vysledek = driver.findElement(By.xpath("//div[contains(@class, 'alert-danger')]"));
-
         String TextVysledku = Vysledek.getText();
+
         Assert.assertEquals(TextVysledku,"Registracia neuspesna!" );
 
-    } @Test
+    }
+    @Test
     public void testEmailVelkaPismo (){
 
-        WebElement Pemail = driver.findElement(By.xpath("//input[@name='email']"));
-        WebElement Pjmeno = driver.findElement(By.xpath("//input[@name='name']"));
-        WebElement Pprimeni = driver.findElement(By.xpath("//input[@name='surname']"));
-        WebElement Pheslo = driver.findElement(By.xpath("//input[@name='password']"));
-        WebElement Preheslo = driver.findElement(By.xpath("//input[@name='password-repeat']"));
-        WebElement SomRobot = driver.findElement(By.id("checkbox"));
-        WebElement Pregistrace = driver.findElement(By.xpath("//button[@class='btn btn-success btn-lg btn-block']"));
+        seleniumReworkBetter.RegistracniFormular.Result result = getResult();
 
-        Pjmeno.sendKeys(Jmeno);
-        Pprimeni.sendKeys(Prijmení);
-        Pemail.sendKeys("NOVA@gmail.com");
-        Pheslo.sendKeys(Heslo);
-        Preheslo.sendKeys(Reheslo);
-        SomRobot.click();
-        Pregistrace.click();
-
-        WebElement Vysledek = driver.findElement(By.xpath("//div[contains(@class, 'alert-success')]"));
-
+        result.Pjmeno.sendKeys(Jmeno);
+        result.Pprimeni.sendKeys(Prijmeni);
+        result.Pemail.sendKeys("NOVA@gmail.com");
+        result.Pheslo.sendKeys(Heslo);
+        result.Preheslo.sendKeys(Reheslo);
+        result.SomRobot.click();
+        result.Pregistrace.click();
+        WebElement Vysledek = driver.findElement(By.xpath("//div[contains(@class, 'success')]"));
         String TextVysledku = Vysledek.getText();
         Assert.assertEquals(TextVysledku,"Registracia uspesna!" );
 
-    } @Test
+    }
+    @Test
     public void testRuzneHela (){
 
-        WebElement Pemail = driver.findElement(By.xpath("//input[@name='email']"));
-        WebElement Pjmeno = driver.findElement(By.xpath("//input[@name='name']"));
-        WebElement Pprimeni = driver.findElement(By.xpath("//input[@name='surname']"));
-        WebElement Pheslo = driver.findElement(By.xpath("//input[@name='password']"));
-        WebElement Preheslo = driver.findElement(By.xpath("//input[@name='password-repeat']"));
-        WebElement SomRobot = driver.findElement(By.id("checkbox"));
-        WebElement Pregistrace = driver.findElement(By.xpath("//button[@class='btn btn-success btn-lg btn-block']"));
 
-        Pheslo.sendKeys(Heslo);
-        Preheslo.sendKeys("fjaio");
-        SomRobot.click();
-        Pregistrace.click();
+        seleniumReworkBetter.RegistracniFormular.Result result = getResult();
 
+        result.Pheslo.sendKeys(Heslo);
+        result.Preheslo.sendKeys("fjaio");
+        result.SomRobot.click();
+        result.Pregistrace.click();
         WebElement Vysledek = driver.findElement(By.xpath("//div[contains(@class, 'alert-danger')]"));
-
         String TextVysledku = Vysledek.getText();
         Assert.assertEquals(TextVysledku,"Registracia neuspesna!" );
 
     } @Test
     public void testHappy (){
 
-        WebElement Pemail = driver.findElement(By.xpath("//input[@name='email']"));
-        WebElement Pjmeno = driver.findElement(By.xpath("//input[@name='name']"));
-        WebElement Pprimeni = driver.findElement(By.xpath("//input[@name='surname']"));
-        WebElement Pheslo = driver.findElement(By.xpath("//input[@name='password']"));
-        WebElement Preheslo = driver.findElement(By.xpath("//input[@name='password-repeat']"));
-        WebElement SomRobot = driver.findElement(By.id("checkbox"));
-        WebElement Pregistrace = driver.findElement(By.xpath("//button[@class='btn btn-success btn-lg btn-block']"));
-
-        Pjmeno.sendKeys(Jmeno);
-        Pprimeni.sendKeys(Prijmení);
-        Pemail.sendKeys(Email);
-        Pheslo.sendKeys(Heslo);
-        Preheslo.sendKeys(Reheslo);
-        SomRobot.click();
-        Pregistrace.click();
-
+        seleniumReworkBetter.RegistracniFormular.Result result = getResult();
+        result.Pjmeno().sendKeys(Jmeno);
+        result.Pprimeni().sendKeys(Prijmeni);
+        result.Pemail().sendKeys("NOVA@gmail");
+        result.Pheslo().sendKeys(Heslo);
+        result.Preheslo().sendKeys(Reheslo);
+        result.SomRobot().click();
+        result.Pregistrace().click();
         WebElement Vysledek = driver.findElement(By.xpath("//div[contains(@class, 'alert-success')]"));
 
         String TextVysledku = Vysledek.getText();
@@ -174,33 +134,46 @@ public class RegistracniFormular {
 
     }
      @Test
-    public void testSpatnyEmailBezPripony(){
+    public void testSpatnyEmailBezPripony() throws InterruptedException, IOException {
 
-        WebElement Pemail = driver.findElement(By.xpath("//input[@name='email']"));
-        WebElement Pjmeno = driver.findElement(By.xpath("//input[@name='name']"));
-        WebElement Pprimeni = driver.findElement(By.xpath("//input[@name='surname']"));
-        WebElement Pheslo = driver.findElement(By.xpath("//input[@name='password']"));
-        WebElement Preheslo = driver.findElement(By.xpath("//input[@name='password-repeat']"));
-        WebElement SomRobot = driver.findElement(By.id("checkbox"));
-        WebElement Pregistrace = driver.findElement(By.xpath("//button[@class='btn btn-success btn-lg btn-block']"));
 
-         Pjmeno.sendKeys(Jmeno);
-         Pprimeni.sendKeys(Prijmení);
-         Pemail.sendKeys("NOVA@gmail");
-         Pheslo.sendKeys(Heslo);
-         Preheslo.sendKeys(Reheslo);
-         SomRobot.click();
-         Pregistrace.click();
+        seleniumReworkBetter.RegistracniFormular.Result result = getResult();
+
+         result.Pjmeno().sendKeys(Jmeno);
+         result.Pprimeni().sendKeys(Prijmeni);
+         result.Pemail().sendKeys("NOVA@gmail");
+         result.Pheslo().sendKeys(Heslo);
+         result.Preheslo().sendKeys(Reheslo);
+         result.SomRobot().click();
+         result.Pregistrace().click();
 
         WebElement Vysledek = driver.findElement(By.xpath("//div[contains(@class, 'alert-success')]"));
 
         String TextVysledku = Vysledek.getText();
         Assert.assertEquals(TextVysledku,"Registracia uspesna!" );
+         File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+         FileUtils.copyFile(scrFile, new File("C:\\Users\\Michal\\AppData\\Local\\Temp\\screenshot.png"));
 
+    }
+
+    private seleniumReworkBetter.RegistracniFormular.Result getResult() {
+        org.openqa.selenium.WebElement Pemail = driver.findElement(org.openqa.selenium.By.xpath("//input[@name='email']"));
+        org.openqa.selenium.WebElement Pjmeno = driver.findElement(org.openqa.selenium.By.xpath("//input[@name='name']"));
+        org.openqa.selenium.WebElement Pprimeni = driver.findElement(org.openqa.selenium.By.xpath("//input[@name='surname']"));
+        org.openqa.selenium.WebElement Pheslo = driver.findElement(org.openqa.selenium.By.xpath("//input[@name='password']"));
+        org.openqa.selenium.WebElement Preheslo = driver.findElement(org.openqa.selenium.By.xpath("//input[@name='password-repeat']"));
+        org.openqa.selenium.WebElement SomRobot = driver.findElement(org.openqa.selenium.By.id("checkbox"));
+        org.openqa.selenium.WebElement Pregistrace = driver.findElement(org.openqa.selenium.By.xpath("//button[@class='btn btn-success btn-lg btn-block']"));
+        seleniumReworkBetter.RegistracniFormular.Result sklad = new seleniumReworkBetter.RegistracniFormular.Result(Pemail, Pjmeno, Pprimeni, Pheslo, Preheslo, SomRobot, Pregistrace);
+        return sklad;
+    }
+
+    private record Result(WebElement Pemail,WebElement Pjmeno,WebElement Pprimeni, WebElement Pheslo, WebElement Preheslo, WebElement SomRobot,WebElement Pregistrace) {
     }
 
     @After
     public void tearDone (){
+
         driver.close();
         driver.quit();
 
